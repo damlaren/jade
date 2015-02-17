d8f6e7f5a4bb55baf6006a73fbe3f06a1e341d46
David McLaren <damlaren@gmail.com>
2015-02-16 23:27:35 -0800

    trying out opencv dense optical flow script but not working yet

---
+++ prototype/dense_optical_flow.py
@@ -0,0 +1,36 @@
+# From: http://docs.opencv.org/trunk/doc/py_tutorials/py_video/py_lucas_kanade/py_lucas_kanade.html
+
+import cv2
+import numpy as np
+cap = cv2.VideoCapture("small.mp4")
+
+ret, frame1 = cap.read()
+prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+hsv = np.zeros_like(frame1)
+hsv[...,1] = 255
+
+while(1):
+    ret, frame2 = cap.read()
+    next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+
+    #Line off OpenCV docs didn't quite work. Instead, see:
+    #http://stackoverflow.com/questions/13685771/opencv-python-calcopticalflowfarneback
+    #flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    flow = cv2.calcOpticalFlowFarneback(prvs,next,0.5,1,3,15,3,5,1)
+
+    mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+    hsv[...,0] = ang*180/np.pi/2
+    hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+    rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+
+    cv2.imshow('frame2',rgb)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+    elif k == ord('s'):
+        cv2.imwrite('opticalfb.png',frame2)
+        cv2.imwrite('opticalhsv.png',rgb)
+    prvs = next
+
+cap.release()
+cv2.destroyAllWindows()
