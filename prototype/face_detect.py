90e330d0f88b4d81668ee5f8f8d74f40f94c037d
David McLaren <damlaren@gmail.com>
2015-02-21 17:53:30 -0800

    face detection code, runs through all images in a directory and detects teh faces

--- prototype/face_detect.py
+++ prototype/face_detect.py
@@ -3,22 +3,44 @@
 
 import numpy as np
 import cv2
+import glob
+import os
+import sys
 
-# TODO: get XML from portable directory-- this is the Haar cascades XML
-# config file, usually found in OpenCV's data directory
-face_cascade = cv2.CascadeClassifier('/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
+# Read script arguments.
+if len(sys.argv) < 3:
+    print "usage: python face_detect.py <data-dir> <output-dir> [xml-dir]\n" \
+          "\tdata-dir: directory containing input data (profile photos)\n" \
+          "\toutput-dir: directory in which to dump output images\n" \
+          "\txml-dir: directory containing XML configuration file\n"
+    sys.exit(1)
 
-img = cv2.imread('f4.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+data_dir = sys.argv[1]
+output_dir = sys.argv[2]
+xml_dir = '/usr/local/share/OpenCV/haarcascades'
+if len(sys.argv) > 3:
+    xml_dir = sys.argv[3]
+print "data directory is: " + data_dir
+print "output directory is: " + output_dir
+print "xml directory is: " + xml_dir
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-face_count = 1
-for (x,y,w,h) in faces:
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    cv2.imwrite('testout' + str(face_count) + '.jpg', img[y:y+h, x:x+w])
-    face_count += 1
+# Create detector.
+face_cascade = cv2.CascadeClassifier(xml_dir + "/haarcascade_frontalface_default.xml")
 
-cv2.imshow('img',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Iterate through all pictures and apply the detection algorithm.
+for file_path in glob.glob(data_dir + "/*.jpg"):
+    img = cv2.imread(file_path)
+    file_name = os.path.basename(file_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    face_index = 1
+    for (x,y,w,h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        output_file_name = "%s/out_%02d_%s" %(output_dir, face_index, file_name)
+        cv2.imwrite(output_file_name, img[y:y+h, x:x+w])
+        face_index += 1
+
+#cv2.imshow('img',img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
