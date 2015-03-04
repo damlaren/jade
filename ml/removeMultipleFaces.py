f67226d64d9ffbe3f6af0ffd7227e1acd3da6f68
Albert <ahaque@cs.stanford.edu>
2015-03-04 02:38:39 -0800

    Updated single label extractor

--- ml/removeMultipleFaces.py
+++ ml/removeMultipleFaces.py
@@ -6,7 +6,8 @@ FACES_DIR = "/home/albert/Desktop/pic5_dataset/faces/"
 SINGLES_DIR = "/home/albert/Desktop/pic5_dataset/singles/"
 
 labels_file = open("all_gender.txt","r")
-output_file = open("histogram.csv", "w")
+histogram_output_file = open("histogram.csv", "w")
+single_labels_output_file = open("pic5_gender_all.txt","w")
 
 lines = labels_file.readlines()
 
@@ -30,7 +31,7 @@ for key in face_counts:
 	if face_counts[key] == 1:
 		single_user_ids.append(key)
 
-count = 0
+"""
 # Copy the .jpg files to the singles folder
 for filename in single_user_ids:
 	# Not all images end with face_0 for some reason...
@@ -41,14 +42,28 @@ for filename in single_user_ids:
 	count += 1
 	dest = SINGLES_DIR + filename + ".jpg"
 	shutil.copy(src, dest)
-print count
-# Copy the labels to the singles folder
+"""
+
+# Create a labels file consisting of the single images only
+gender_labels = {}
+for line in lines:
+	tokens = line.strip().split(' ')
+	gender_labels[tokens[0]] = int(tokens[1])
+
+# Only write the single labels
+for key in single_user_ids:
+	key = key + ".jpg"
+	# Some user_ids won't have a gender label
+	if key not in gender_labels:
+		continue
+	# Write to file
+	single_labels_output_file.write(key + " " + str(gender_labels[key]) + "\n")
 
 """
 # Creates and prints out the histogram (this code works perfectly fine)
 histogram = {}
 for key in face_counts:
-	#output_file.write(key + "," + str(face_counts[key]) + "\n")
+	#histogram_output_file.write(key + "," + str(face_counts[key]) + "\n")
 	if face_counts[key] not in histogram:
 		histogram[face_counts[key]] = 1
 	else:
@@ -57,5 +72,6 @@ for key in face_counts:
 print histogram
 """
 
-output_file.close()
+histogram_output_file.close()
 labels_file.close()
+single_labels_output_file.close()
