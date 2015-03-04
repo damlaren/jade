ec239f275e3607dd949fba7e35c47c9346d35660
Albert <ahaque@cs.stanford.edu>
2015-03-04 15:51:40 -0800

    Better comments for removeMultipleFaces.py

--- ml/removeMultipleFaces.py
+++ ml/removeMultipleFaces.py
@@ -6,11 +6,14 @@ FACES_DIR = "/home/albert/Desktop/pic5_dataset/faces/"
 SINGLES_DIR = "/home/albert/Desktop/pic5_dataset/singles/"
 
 labels_file = open("all_gender.txt","r")
-histogram_output_file = open("histogram.csv", "w")
 single_labels_output_file = open("pic5_gender_all.txt","w")
 
 lines = labels_file.readlines()
 
+
+
+# This loop opens the FACES_DIR folder and counts the number of times
+# a face appears per image.
 file_list = os.listdir(FACES_DIR)
 face_counts = {}
 
@@ -31,6 +34,7 @@ for key in face_counts:
 	if face_counts[key] == 1:
 		single_user_ids.append(key)
 
+
 """
 # Copy the .jpg files to the singles folder
 for filename in single_user_ids:
@@ -45,12 +49,14 @@ for filename in single_user_ids:
 """
 
 # Create a labels file consisting of the single images only
+# This loop below reads the gender labels for the ENTIRE dataset into gender_labels
 gender_labels = {}
 for line in lines:
 	tokens = line.strip().split(' ')
 	gender_labels[tokens[0]] = int(tokens[1])
 
-# Only write the single labels
+# Only write the single labels. We then loop through our
+# singles array and get the correct label from the ALL gender_labels dict
 for key in single_user_ids:
 	key = key + ".jpg"
 	# Some user_ids won't have a gender label
@@ -59,8 +65,12 @@ for key in single_user_ids:
 	# Write to file
 	single_labels_output_file.write(key + " " + str(gender_labels[key]) + "\n")
 
+
 """
 # Creates and prints out the histogram (this code works perfectly fine)
+# If you're curious about how many images we lost, etc.
+
+histogram_output_file = open("histogram.csv", "w")
 histogram = {}
 for key in face_counts:
 	#histogram_output_file.write(key + "," + str(face_counts[key]) + "\n")
@@ -70,8 +80,9 @@ for key in face_counts:
 		histogram[face_counts[key]] += 1
 
 print histogram
+histogram_output_file.close()
+
 """
 
-histogram_output_file.close()
 labels_file.close()
 single_labels_output_file.close()
