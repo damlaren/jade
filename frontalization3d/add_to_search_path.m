5a6f8808cf8cca962cd7933b0e5936bed185c7a0
David Alan McLaren <dmclaren@rye02.stanford.edu>
2015-03-13 11:44:32 -0700

    Got the demo running in MATLAB. Added a README with setup instructions.

--- frontalization3d/add_to_search_path.m
+++ frontalization3d/add_to_search_path.m
@@ -1,6 +1,10 @@
 % CS231A Project File
 % Set up search path.
 
-path('C:\Projects\jade\frontalization.0.1.1\calib', path);
-path('C:\Projects\jade\frontalization.0.1.1\face-release1.0-basic', path);
-path('C:\opencv', path);
+%path('C:\Projects\jade\frontalization3d\calib', path);
+%path('C:\Projects\jade\frontalization3d\face-release1.0-basic', path);
+%path('C:\opencv', path);
+
+path('~/projects/jade/frontalization3d/calib', path);
+path('~/projects/jade/frontalization3d/face-release1.0-basic', path);
+path('~/projects/opencv-install', path);
